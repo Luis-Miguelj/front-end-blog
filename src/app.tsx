cd92dba01d431901d1ece3c38c77ecd994dc2937
Luis-Miguelj <luismiguel.dev01@gmail.com>
2024-03-03 23:02:50 -0300

    Teste 2

--- src/app.tsx
+++ src/app.tsx
@@ -47,7 +47,7 @@ export function App() {
         <Tabs />
       </div>
       <main
-        className="py-5 px-10 max-w-6xl mx-auto max-h-[450px] rounded-md space-y-5 overflow-y-auto overflow-hidden scroll-smooth"
+        className="py-5 px-10 max-w-6xl mx-auto max-h-[450px] max-md:max-h-[550px] rounded-md space-y-5 overflow-y-auto overflow-hidden scroll-smooth"
         id="scroll-posts"
       >
         <div className="flex flex-col ">
